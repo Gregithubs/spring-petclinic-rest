ad17be48e14a240eefb8585b10a02efb4ec1a8c8
Gregithubs <greg.pateyron@gmail.com>
2022-10-18 18:22:45 +0200

    modif

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -11,3 +11,35 @@ public class PetClinicApplication extends SpringBootServletInitializer {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 }
+public class StreamLambdaHandler implements RequestStreamHandler {
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+            // If you are using HTTP APIs with the version 2.0 of the proxy model, use the getHttpApiV2ProxyHandler
+            // method: handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Application.class);
+        } catch (ContainerInitializationException e) {
+            // if we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
+@SpringBootApplication
+@Import({ PetsController.class })
+public class Application {
+
+    // silence console logging
+    @Value("${logging.level.root:OFF}")
+    String message = "";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
